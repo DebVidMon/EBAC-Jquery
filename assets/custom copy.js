29342f9c534b98ae21a3c836aba5e1fdcc97d84f
Debora <debvm@hotmail.com>
2022-07-18 02:46:39 -0300

    uniformizando arquivo copy com o original

--- assets/custom copy.js
+++ assets/custom copy.js
@@ -3,6 +3,42 @@ $(document).ready(function () {
   $(document).find(".text-muted").hide();
   $(document).find(".text-muted1").hide();
 
+  $('.owl-carousel').owlCarousel();
+  
+  let titulos = $('h4') // tag
+  
+  let itens = $('.featured-item') // class
+  
+  let destaques = $('#featured') // id
+  // console.log(titulos.first());
+  
+  // Configuração de produtos
+  
+  $('.featured-item a').addClass('btn btn-dark stretch-link');
+  
+  $('.featured-item:first h4').append('<span class="badge bg-secondary">Novo</span>')
+  
+  $('.featured-item h4').dblclick( function(){
+     
+     $(this).css({
+        'color': '#f00',
+        'background': '#ff0',
+        'font-weight': '100',
+     });
+     
+  });
+  
+  /*
+   * Manipulação de eventos
+   */
+  $('.featured-item a').on('blur', function(event){
+     
+     event.preventDefault();
+     
+     alert('Produto esgotado');
+     
+  })
+
   /* Ouvinte de eventos .nav-modal-open */
   $(".nav-modal-open").on("click", function (e) {
     e.preventDefault();
